69ffdce76827ec4ba9e190806d673bb5e52d6bea
Stina <3kdapp25.thunst@folkuniversitetet.nu>
2025-10-22 13:52:56 +0200

    init scanner och starta isValidInput

--- src/utils/InputHandler.java
+++ src/utils/InputHandler.java
@@ -1,4 +1,26 @@
 package utils;
 
+import java.util.Scanner;
+
 public class InputHandler {
+    private Scanner sc;
+
+    public  InputHandler(Scanner scanner) {
+        this.sc = sc;
+    }
+
+    public String getInput() {
+        System.out.println("Viken ruta vill du undersöka?");
+        return sc.nextLine().toLowerCase();
+    }
+    public boolean isValidInput(String input) {
+        if (input.length() != 2)
+            return false;
+        // todo symbol, inom bounds
+        return true;
+    }
+
+
+
+
 }
