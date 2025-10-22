51de61d660135401230b5351badd5bf0d27cee09
Stina <3kdapp25.thunst@folkuniversitetet.nu>
2025-10-22 14:07:23 +0200

    skrev todo

--- src/utils/InputHandler.java
+++ src/utils/InputHandler.java
@@ -13,12 +13,18 @@ public class InputHandler {
         System.out.println("Viken ruta vill du undersöka?");
         return sc.nextLine().toLowerCase();
     }
-    public boolean isValidInput(String input) {
+    public boolean isValidInput(String input, int maxRow, int maxCol) {
         if (input.length() != 2)
             return false;
         // todo symbol, inom bounds
         return true;
     }
+
+    //todo int till rad
+    //Todo int till kolumn
+
+
+
 
 
 
