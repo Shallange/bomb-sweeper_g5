8750796bb5268d385ec2a90ccfe0846358809f5c
Shallange <jimmy.kroneld@hotmail.com>
2025-10-22 15:59:48 +0200

    Updated Bomb class with fields and javadoc

--- src/dangers/Bomb.java
+++ src/dangers/Bomb.java
@@ -1,4 +1,23 @@
 package dangers;
 
+/**
+ * Represents a bomb on the grid with its coordinates
+ */
 public class Bomb {
+    private int row;
+    private int col;
+
+    /**
+     * Creates a New Bomb at given row and col
+     *
+     * @param row rows index on the gamegrid
+     * @param col col index on the gamegrid
+     */
+    public Bomb(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getCol() {return col;}
+    public int getRow() {return row;}
 }
