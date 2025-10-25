9958963013d8bc8be77b78590803202a8b95657f
Stina <3kdapp25.thunst@folkuniversitetet.nu>
2025-10-25 10:05:31 +0200

    init "flag"

--- src/utils/Emoji.java
+++ src/utils/Emoji.java
@@ -5,4 +5,5 @@ public class Emoji {
     public static final String collision = "\uD83D\uDCA5";
     public static final String kross = "\u274E";
     public static final String partyPopper = "\u1F389";
+    public static final String flag = "\uD83D\uDEA9";
 }
