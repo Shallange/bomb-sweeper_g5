ac9a01b859f5f39d4894647a3c1ffdcb49fdd1d7
Shallange <jimmy.kroneld@hotmail.com>
2025-10-26 15:21:31 +0100

    Added RatioCalculator class for scaling calculations

---
+++ src/utils/RatioCalculator.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class RatioCalculator {
+
+    private double ratio;
+
+    public RatioCalculator(double ratio){
+        this.ratio = ratio;
+    }
+
+    public double getRatio() {return ratio;}
+    public void setRatio(double ratio) {this.ratio = ratio;}
+}
