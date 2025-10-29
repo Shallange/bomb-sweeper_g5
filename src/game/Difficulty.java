1eaa8d3cf0ad9f08b66221bc4d25254b195278ae
Shallange <jimmy.kroneld@hotmail.com>
2025-10-29 15:49:24 +0100

    Added difficulty logic and fixed integration in Game

--- src/game/Difficulty.java
+++ src/game/Difficulty.java
@@ -7,14 +7,50 @@ public class Difficulty {
     private int rows;
     private int cols;
     private String name; // EASY, MEDIUM and HARD
+    private int numberOfBombs;
 
-    public Difficulty(int difficulty, String name){
+    RatioCalculator easy = new RatioCalculator(0.15);
+    RatioCalculator medium = new RatioCalculator(0.22);
+    RatioCalculator hard = new RatioCalculator(0.30);
+
+
+    public Difficulty(int difficulty){
         this.difficulty = difficulty;
-        this.name = name;
+        chooseDifficulty(this.difficulty);
     }
 
-    public int getDifficulty() {return difficulty;}
-    public void setDifficulty(int difficulty) {this.difficulty = difficulty;}
+    /**
+     * Sets the difficulty of the game based on userinput (1-3)
+     * Each difficulty sets
+     *  - grid size
+     *  - Number of bombs
+     *
+     * @param difficulty is an int value representing difficulty level: 1 = easy, 2 = medium and 3 = hard
+     */
+    public void chooseDifficulty(int difficulty){
+        switch (difficulty){
+            case 1:
+                this.setName("EASY");
+                this.setCols(5);
+                this.setRows(5);
+                setNumberOfBombs(easy.calculateNumOfBombs(this.getCols(), this.getRows()));
+                break;
+            case 2:
+                this.setName("MEDIUM");
+                this.setCols(7);
+                this.setRows(7);
+                setNumberOfBombs(medium.calculateNumOfBombs(this.getCols(), this.getRows()));
+                break;
+            case 3:
+                this.setName("HARD");
+                this.setCols(9);
+                this.setRows(9);
+                setNumberOfBombs(hard.calculateNumOfBombs(this.getCols(), this.getRows()));
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+    }
 
     public int getRows() {return rows;}
     public void setRows(int rows) {this.rows = rows;}
@@ -23,5 +59,9 @@ public class Difficulty {
     public void setCols(int cols) {this.cols = cols;}
 
     public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    private void setName(String name) {this.name = name;}
+
+    public int getNumberOfBombs() {return numberOfBombs;}
+
+    public void setNumberOfBombs(int numberOfBombs) {this.numberOfBombs = numberOfBombs;}
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -2,56 +2,32 @@ package game;
 import dangers.Bomb;
 import dangers.BombPlacer;
 import utils.InputHandler;
-import utils.RatioCalculator;
 import utils.Emoji;
 import utils.Color;
 
 import java.util.List;
 
 public class Game {
-    int rows = 6;
-    int cols = 6;
-    int numBombs;
-    boolean [][] revealed = new boolean[rows][cols];
 
+    int numBombs;
     InputHandler inputHandler = new InputHandler();
     BombPlacer bombPlacer = new BombPlacer();
-    RatioCalculator calc = new RatioCalculator();
-
-    private int countRevealed() {
-        int revealedCount = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (revealed[i][j]) {
-                    revealedCount++;
-                }
-            }
-        }
-        return revealedCount;
-    }
-
-    private boolean checkWin() {
-        if (countRevealed() + numBombs == rows * cols) {
-            System.out.println(Emoji.partyPopper + Color.green + "Grattis! Du har klarat spelet!!!" + Color.reset + Emoji.happy);
-            return true;
-        }
-        return false;
-    }
-
 
     public void playGame() {
         boolean playAgain = true;
 
         while (playAgain) {
-            calc.setRatio(0.15);
-            numBombs = calc.calculateNumOfBombs(rows, cols);
-            List<Bomb> bombs = bombPlacer.placeBombs(rows, cols, numBombs);
-            Table table = new Table(rows, cols, bombs);
+            Difficulty diff = new Difficulty(3);//userinput
+            boolean [][] revealed = new boolean[diff.getRows()][diff.getCols()];
+            numBombs = diff.getNumberOfBombs();
+            List<Bomb> bombs = bombPlacer.placeBombs(diff.getRows(), diff.getCols(), numBombs);
+            Table table = new Table(diff.getRows(), diff.getCols(), bombs);
             table.showTable();
+            int countNumber = countRevealed(diff.getRows(), diff.getCols(),revealed);
+            int totalNumberOfCells = diff.getCols() * diff.getRows();
 
         while (true) {
-            String input = inputHandler.getInput(rows,cols);
-
+            String input = inputHandler.getInput(diff.getRows(), diff.getCols());
             int row = inputHandler.rowIndex(input);
             int col = inputHandler.colIndex(input);
 
@@ -65,24 +41,46 @@ public class Game {
                 table.table[row][col] = " " + Emoji.kross + " ";
             }
 
+
             if (revealed[row][col]) {
                 System.out.println(Emoji.collision + Color.red + "Rutan är redan undersökt, försök med en annan: " + Color.reset);
                 continue;
             }
-
                 revealed[row][col] = true;
                 table.showTable();
-                if (checkWin()) break;
-
+                if (checkWin(countNumber, totalNumberOfCells)) break;
             }
              playAgain = askPlayAgain();
         }
         System.out.println("Tack för att du spelade!");
     }
 
+
     private boolean askPlayAgain() {
         System.out.println("Vill du spela igen? (y/n)");
         String answer = InputHandler.sc.nextLine().trim().toLowerCase();
         return answer.equals("y");
+    }
+
+
+    private int countRevealed(int row, int col, boolean [][] revealed) {
+        int revealedCount = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (revealed[i][j]) {
+                    revealedCount++;
+                }
+            }
+        }
+        return revealedCount;
+    }
+
+
+    private boolean checkWin(int countNumber,int totalNumOfCells) {
+        if (countNumber == totalNumOfCells) {
+            System.out.println(Emoji.partyPopper + Color.green + "Grattis! Du har klarat spelet!!!" + Color.reset + Emoji.happy);
+            return true;
+        }
+        return false;
     }
 }
